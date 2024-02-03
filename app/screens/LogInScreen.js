73990a7e4506b3ab0e41b2091e63ef398ff7102e
Abdellah <you@example.com>
2024-02-03 16:37:20 +0100

    test

---
+++ app/screens/LogInScreen.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { Button, StyleSheet, Text, TextInput, View, Alert, Image } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+
+// Constants
+const Separator = () => <View style={styles.separator} />;
+
+function LogInScreen(props) {
+    return (
+    <LinearGradient
+        colors={['#FFF1EB', '#7A9E9F']}
+        style={styles.container}>
+
+        <Image style={styles.BigIcon} source={require('../assets/icons/UserIcon.png')} />
+        
+        <Separator />
+
+        {/* Inputs */}
+        <TextInput style={styles.input} placeholder='User Name'/>
+        <TextInput style={styles.input} placeholder='Password'/>
+
+        {/* Touchable texts */}
+        <View style={styles.row}>
+            <Text style={styles.text}>Remember Me </Text>
+            <Text style={styles.text}>Forgot Password?</Text>
+        </View>
+
+        <Separator />
+
+        <Button title='Log In' onPress={() => Alert.alert('Logged In!')} />
+
+        <Separator />
+
+        <Text style={styles.text}>You don't have an account ?</Text>
+        <Text style={styles.text, {color: '#FFF'}}>SIGN UP</Text>
+    </LinearGradient>
+    );
+}
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      padding: 15,
+    },
+    BigIcon: {
+        width: 100,
+        height: 100,
+    },
+    input:{
+      width: 300,
+      height: 40,
+      margin: 12,
+      borderRadius: 30,
+      borderWidth: 1,
+      padding: 10,
+      backgroundColor: '#FFF1EB',
+    },
+    separator: {
+      marginVertical: 10,
+    },
+    row: {
+    //backgroundColor: 'red',
+    width: 280,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    },
+  });
+
+export default LogInScreen;
