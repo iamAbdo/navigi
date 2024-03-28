d96bbfd853bfb92d6ac8f7d7c48b34fa1776fdc9
Abdellah <you@example.com>
2024-03-28 07:09:03 +0100

    Working Cards!

--- App.js
+++ App.js
@@ -1,31 +1,50 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import {StyleSheet} from 'react-native';
+import { View, Text, Image, StyleSheet, ScrollView } from 'react-native';
 
-import LogInScreen from './app/screens/LogInScreen';
-import HomeScreen from './app/screens/HomeScreen';
+// Unused imports are comment for now
+// import LogInScreen from './app/screens/LogInScreen';
+// import HomeScreen from './app/screens/HomeScreen';
 import MediaCard from './app/screens/components/card';
 
 export default function App() {
-
   return (
-    <div style={{backgroundColor: 'red'}}>
-      <MediaCard 
-        title= 'Informatique'
-        image= 'Categories/informatique.jfif' // inside assets
-        description={'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica'}
-      />
-      <MediaCard 
-        title= 'ARCHITECT'
-        image= 'Categories/informatique.jfif' // inside assets
-        description={'what ever description we want to put here!'}
-      />
-      <MediaCard 
-        title= 'HAIR STYLIST'
-        image= 'Categories/informatique.jfif' // inside assets
-        description={'i think that the description is gotten from a backend api server with a key!'}
-      />
-    </div>
+    <View style={styles.pageContainer}>
+      <View style={styles.container}>
+        <ScrollView horizontal contentContainerStyle={{gap: 10}}>
+            <MediaCard style={{ marginRight: 20 }}
+              title='Informatique'
+              image='Categories/informatique.jfif' // inside assets
+              description={'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica'}
+            />
+            <MediaCard 
+              title='ARCHITECT'
+              image='Categories/informatique.jfif' // inside assets
+              description={'what ever description we want to put here!'}
+            />
+            <MediaCard 
+              title='HAIR STYLIST'
+              image='Categories/informatique.jfif' // inside assets
+              description={'i think that the description is gotten from a backend api server with a key!'}
+            />
+        </ScrollView>
+      </View>
+      <StatusBar style="auto" />
+    </View>
   );
-
 }
 
+const styles = StyleSheet.create({
+  pageContainer: {
+    backgroundColor: '#B8D8D8',
+    flex: 1,
+    alignItems: 'center', // Adjust this as needed for horizontal alignment
+  },
+  container: {
+    display: 'flex',
+    marginTop: 30,
+    flexDirection: 'row', // Change to 'column' if you want vertical stacking
+    justifyContent: 'space-between', // Adjust this as needed for spacing between cards
+    alignItems: 'center', // Adjust this as needed for vertical alignment
+  },
+});
--- app/screens/components/card.js
+++ app/screens/components/card.js
@@ -1,46 +1,14 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
+import React from 'react';
+import { Card, Title, Paragraph } from 'react-native-paper';
 
-import { CardActionArea } from '@mui/material';
-
-/* could be used now they are not */
-import CardActions from '@mui/material/CardActions';
-import Button from '@mui/material/Button';
-
-export default function MediaCard({ title, image, description}) {
+export default function MediaCard({ title, image, description }) {
   return (
-    <Card sx={{ maxWidth: 345 }}>
-       <CardActionArea>
-        <CardMedia
-          sx={{ height: 140 }}
-          image={`/app/assets/${image}`}
-          title="Image"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {title}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {description}
-          </Typography>
-        </CardContent>
-
-
-      
-        {/*
-
-        Action can be on card click or on this actions click
-
-        <CardActions>
-          <Button size="small">Share</Button>
-          <Button size="small">Learn More</Button>
-        </CardActions>
-        */}
-
-      </CardActionArea>
+    <Card style={{ maxWidth: 345, backgroundColor: '#7A9E9F' }}>
+      <Card.Cover source={{ uri: `/app/assets/${image}` }} />
+      <Card.Content style={{ height: 100 }}>
+        <Title style={{ fontWeight: 'bold' }}>{title}</Title>
+        <Paragraph style={{ color: '#333' }}>{description}</Paragraph>
+      </Card.Content>
     </Card>
   );
 }
