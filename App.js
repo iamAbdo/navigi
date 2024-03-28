4088f53692c8f6cf0233375e73917d95d0a9d628
Abdellah <you@example.com>
2024-03-28 14:45:57 +0100

    navigationsss

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { useNavigation } from '@react-navigation/native';
 
 // Unused imports are comment for now
 import LogInScreen from './app/screens/LogInScreen';
@@ -14,7 +13,7 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="Home" screenOptions={{headerShown: false}} >
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="LogInScreen" component={LogInScreen} />
       </Stack.Navigator>
